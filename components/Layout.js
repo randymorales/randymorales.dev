6dd2963846861b2a519c8e589e3ba2ddc9abc54f
Randy <randymoralesg@gmail.com>
2020-12-22 17:15:57 -0600

    Change favicon
    Add web, Android, Apple and Microsoft icons
    and their configuration
    
    Signed-off-by: Randy <randymoralesg@gmail.com>

--- components/Layout.js
+++ components/Layout.js
@@ -16,15 +16,34 @@ export default function Layout({ children, home }) {
       <LanguageSelector />
 
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='/android-icon-192x192.png'
+        />
+        <link rel='manifest' href='/manifest.json' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+
+        <meta name='theme-color' content='#ffffff' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='msapplication-TileImage' content='/ms-icon-310x310.png' />
+
         <meta name='description' content={t('slogan')} />
+        <meta name='og:title' content={SiteTitle} />
+        <meta name='og:description' content={t('slogan')} />
         <meta
           property='og:image'
           content={`https://og-image.now.sh/${encodeURI(
             SiteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name='og:title' content={SiteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
 
