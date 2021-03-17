22099e4cd77fe446bee6276292a38463cb4f86b2
Randy <randymoralesg@gmail.com>
2021-03-16 21:29:35 -0600

    Add about page
    Signed-off-by: Randy <randymoralesg@gmail.com>

--- i18n/strings.js
+++ i18n/strings.js
@@ -6,8 +6,21 @@ const LangStrings = {
     'intro-description':
       "I'm a software engineer writing these posts and building open-source projects.",
     blog: 'Blog',
-    'blog-description':
-      "Hi! I'm a software engineer. This website is my digital legacy — a compendium of the things I've learned and put into practice.",
+    'about-paragraph1':
+      'I am a software engineer based on Costa Rica 🇨🇷, graduated from TEC - Costa Rica Institute of Technology.',
+    'about-paragraph2':
+      'Professionally, I am working at Aruba Networks, programming features in the switch\
+      operating system.',
+    'about-paragraph3':
+      'Personally, I like to exercise (football, MTB, gym) 🚵, play video games 🎮, and read 📖.\
+      Music? Mostly “classic rock” 🎸.',
+    'about-paragraph4':
+      "This website is the fusion of the professional and personal world. I want to use this\
+      website to learn and teach in public about what I discover throughout my career since I \
+      consider myself as a 'forever student'. My long-term goal is to be a software architect \
+      and I hope that journey will be documented here.",
+    'about-paragraph5':
+      'Feel free to reach me out and I hope you enjoy my blog posts 📝.',
     'error-content': 'Oops something went wrong. Check the requested link.',
     'error-heading': 'Error',
     greeting: "Hello! I'm ",
@@ -18,6 +31,7 @@ const LangStrings = {
     'posts-found': 'Posts found',
     'posts-tag': 'Tag',
     'read-more': 'Read more',
+    'skills-tools': 'Skills - tools I’ve used',
     slogan: 'Randy Morales - Programming Blog',
     sorry: 'Sorry',
     translate: 'Traducir 🇪🇸',
@@ -29,8 +43,19 @@ const LangStrings = {
     'intro-description':
       'Soy un ingeniero de software que crea proyectos de código abierto y escribe sobre programación.',
     blog: 'Blog',
-    'blog-description':
-      'Hola! Soy un ingeniero de software. Este sitio web es mi espacio digital — conocimiento que he adquirido y puesto en práctica.',
+    'about-paragraph1':
+      'Soy un ingeniero de software ubicado en Costa Rica 🇨🇷, graduado del TEC - Instituto Tecnológico de Costa Rica.',
+    'about-paragraph2':
+      'Profesionalmente, trabajo en Aruba Networks, programando features en el sistema operativo del switch.',
+    'about-paragraph3':
+      'Personalmente, me gusta el ejercicio (fútbol, MTB, gimnasio) 🚵, jugar video juegos 🎮, y leer 📖.\
+      Música? Mayormente “rock clásico” 🎸.',
+    'about-paragraph4':
+      "Este sitio web es la fusión del mundo profesional y el personal. Quiero usar este sitio para aprender y enseñar\
+      en público acerca de lo que descubro a lo largo de mi carrera, ya que me considero un 'estudiante por siempre'. \
+      Mi meta a largo plazo es ser arquitecto de software y espero que ese recorrido sea documentado aquí.",
+    'about-paragraph5':
+      'Siéntase libre de contactarme y espero que disfrute los posts 📝.',
     'error-content': 'Algo salió mal. Revise el link accesado.',
     'error-heading': 'Error',
     greeting: '¡Hola! Soy ',
@@ -41,6 +66,7 @@ const LangStrings = {
     'posts-found': 'Posts encontrados',
     'posts-tag': 'Etiqueta',
     'read-more': 'Leer más',
+    'skills-tools': 'Skills - tools que he usado',
     slogan: 'Randy Morales - Blog de Programación',
     sorry: 'Lo siento',
     translate: 'Translate 🇬🇧',
--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,8 @@ import { Name } from '@/lib/constants'
 import useTranslation from '@/i18n/useTranslation'
 import Layout from '@/components/Layout'
 
+import styles from '@/styles/about.module.css'
+
 export default function About() {
   const { t } = useTranslation()
 
@@ -12,7 +14,37 @@ export default function About() {
           {t('greeting')} {Name}
         </h1>
 
-        <p>{t('blog-description')}</p>
+        <div className={styles.container}>
+          <div className={styles.leftSide}>
+            <p className={styles.paragraph}>{t('about-paragraph1')}</p>
+
+            <p className={styles.paragraph}>{t('about-paragraph2')}</p>
+
+            <p className={styles.paragraph}>{t('about-paragraph3')}</p>
+
+            <p className={styles.paragraph}>{t('about-paragraph4')}</p>
+
+            <p className={styles.paragraph}>{t('about-paragraph5')}</p>
+
+            <h3 className={styles.subtitle}>{t('skills-tools')}</h3>
+            <ul className={styles.skills}>
+              <li>Go</li>
+              <li>C</li>
+              <li>Linux</li>
+              <li>Python</li>
+              <li>Javascript</li>
+              <li>NextJS</li>
+              <li>Git</li>
+            </ul>
+          </div>
+
+          <div className={styles.rightSide}>
+            <img
+              src='./images/college-graduation.jpg'
+              alt='college graduation photo'
+            />
+          </div>
+        </div>
       </Layout>
     </>
   )
