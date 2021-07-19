f272b0e6a32c9e48ca06c637b2863b449e7a74aa
Randy <randymoralesg@gmail.com>
2021-07-19 00:00:24 -0600

    Make GA Tracking ID public
    Make GA Tracking ID public to NextJS deployment,
    so that it will be visible to the browser.
    
    Signed-off-by: Randy <randymoralesg@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,7 @@ export default class MyDocument extends Document {
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
           />
           <script
             dangerouslySetInnerHTML={{
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${process.env.GA_TRACKING_ID}');
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
               `,
             }}
           />
